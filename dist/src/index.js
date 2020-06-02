8743a6659a2a5df13e8905f95840db10c376498c
wkimak <wkimak94@gmail.com>
2020-06-02 19:53:02 -0400

    Updated terminal messages

--- dist/src/index.js
+++ dist/src/index.js
@@ -15,11 +15,12 @@ let masterOptionUsed;
 let sourceFileName;
 let targetFileName;
 function checkFileOption(argv) {
+    console.log('arv', argv);
     sourceFileName = argv.file;
     buildCommandUsed = argv._[0] === 'build';
     targetFileName = `${sourceFileName.split('.').slice(0, -1).join('.')}.spec.ts`;
     if (!regex_1.isComponentFile.test(sourceFileName) && !regex_1.isServiceFile.test(sourceFileName) && !regex_1.isResourceFile.test(sourceFileName)) {
-        throw new Error(`Invalid file name: ${sourceFileName}. The file name must have an extension of component, service, or resource.`);
+        throw new Error(`Invalid file name: ${sourceFileName}. The source file name must have an extension of .component, .service , or .resource (e.g. app.component.ts, app.service.ts, app.resource.ts).`);
     }
     else if (buildCommandUsed && fs_1.default.existsSync(targetFileName)) {
         throw new Error(`${targetFileName} file already exists. Use the update command to update a spec file’s providers.`);
@@ -35,37 +36,39 @@ function checkMasterOption(argv) {
         const rootDirectory = stubDependencies_1.findRootDirectory(process.cwd());
         const masterFilePath = stubDependencies_1.searchFileSystem('MasterServiceStub.ts', rootDirectory);
         if (!masterFilePath) {
-            throw new Error('MasterServiceStub.ts file not found. The MasterServiceStub’s file must be named MasterServiceStub.ts');
+            throw new Error('MasterServiceStub.ts file not found. The MasterServiceStub’s file must be named MasterServiceStub.ts.');
         }
     }
     return true;
 }
-yargs_1.default.usage('Usage: $0 <command> [options]')
-    .command('build', 'Build test file')
-    .example('$0 build -f app.component.ts', 'build test file for app.component.ts')
-    .command('update', 'Update providers')
-    .example('$0 update -f app.component.ts', 'update providers configuration')
+yargs_1.default.command('build', 'Build a non-existing spec file')
+    .example('$0 build -f app.component.ts')
+    .command('update', 'Update an existing spec file\'s providers')
+    .example('$0 update -f app.component.ts')
     .option('f', {
     alias: 'file',
     demandOption: true,
     nargs: 1,
-    describe: 'Load a File',
+    describe: 'Load specified file',
     type: 'string',
     global: true
 })
     .check((argv) => {
     return checkFileOption(argv);
 })
+    .demandCommand(1, 'You need at least one command before moving on')
     .option('m', {
     alias: 'master',
     demandOption: false,
     nargs: 0,
-    describe: 'Use MasterServiceStub',
+    describe: 'Configure spec file to use a MasterServiceStub (master class to hold instances of all service stubs). This can be a good approach to ensure new instances of every service stub before each test.',
     global: true
 })
     .check((argv) => {
     return checkMasterOption(argv);
 })
+    .example('$0 build -f app.component.ts -m')
+    .example('$0 update -f app.component.ts -m')
     .help('h')
     .alias('h', 'help').argv;
 writeFile_1.default(buildCommandUsed, masterOptionUsed, sourceFileName, targetFileName);
--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ function checkFileOption(argv): boolean {
   targetFileName = `${sourceFileName.split('.').slice(0, -1).join('.')}.spec.ts`;
 
   if (!isComponentFile.test(sourceFileName) && !isServiceFile.test(sourceFileName) && !isResourceFile.test(sourceFileName)) {
-    throw new Error(`Invalid file name: ${sourceFileName}. The file name must have an extension of component, service, or resource.`);
+    throw new Error(`Invalid file name: ${sourceFileName}. The source file name must have an extension of .component, .service , or .resource (e.g. app.component.ts, app.service.ts, app.resource.ts).`);
   } else if (buildCommandUsed && fs.existsSync(targetFileName)) {
     throw new Error(`${targetFileName} file already exists. Use the update command to update a spec file’s providers.`);
   } else if (!buildCommandUsed && !fs.existsSync(targetFileName)) {
@@ -34,39 +34,41 @@ function checkMasterOption(argv): boolean {
     const rootDirectory = findRootDirectory(process.cwd());
     const masterFilePath = searchFileSystem('MasterServiceStub.ts', rootDirectory);
     if (!masterFilePath) {
-      throw new Error('MasterServiceStub.ts file not found. The MasterServiceStub’s file must be named MasterServiceStub.ts');
+      throw new Error('MasterServiceStub.ts file not found. The MasterServiceStub’s file must be named MasterServiceStub.ts.');
     }
   }
 
   return true;
 }
 
-argv.usage('Usage: $0 <command> [options]')
-  .command('build', 'Build test file')
-  .example('$0 build -f app.component.ts', 'build test file for app.component.ts')
-  .command('update', 'Update providers')
-  .example('$0 update -f app.component.ts', 'update providers configuration')
+argv.command('build', 'Build a non-existing spec file')
+  .example('$0 build -f app.component.ts')
+  .command('update', 'Update an existing spec file\'s providers')
+  .example('$0 update -f app.component.ts')
   .option('f', {
     alias: 'file',
     demandOption: true,
     nargs: 1,
-    describe: 'Load a File',
+    describe: 'Load specified file',
     type: 'string',
     global: true
   })
   .check((argv) => {
     return checkFileOption(argv);
   })
+  .demandCommand(1, 'You need at least one command before moving on')
   .option('m', {
     alias: 'master',
     demandOption: false,
     nargs: 0,
-    describe: 'Use MasterServiceStub',
+    describe: 'Configure spec file to use a MasterServiceStub (master class to hold instances of all service stubs). This can be a good approach to ensure new instances of every service stub before each test.',
     global: true
   })
   .check((argv) => {
     return checkMasterOption(argv);
   })
+  .example('$0 build -f app.component.ts -m')
+  .example('$0 update -f app.component.ts -m')
   .help('h')
   .alias('h', 'help').argv;
 
