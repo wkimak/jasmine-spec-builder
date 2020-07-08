db8a16703d80a263e59107b769614fcc1489b7fe
wkimak <wkimak94@gmail.com>
2020-07-08 17:42:10 -0400

    removed global option props

--- src/index.ts
+++ src/index.ts
@@ -50,8 +50,7 @@ argv.command('build', 'Build a non-existing spec file')
     demandOption: true,
     nargs: 1,
     describe: 'Load specified file',
-    type: 'string',
-    global: true
+    type: 'string'
   })
   .check((argv) => {
     return checkFileOption(argv);
@@ -61,8 +60,7 @@ argv.command('build', 'Build a non-existing spec file')
     alias: 'master',
     demandOption: false,
     nargs: 0,
-    describe: 'Configure spec file to use a MasterServiceStub (master class to hold instances of all service stubs). This can be a good approach to ensure new instances of every service stub before each test.',
-    global: true
+    describe: 'Configure spec file to use a MasterServiceStub (master class to hold instances of all service stubs). This can be a good approach to ensure new instances of every service stub before each test.'
   })
   .check((argv) => {
     return checkMasterOption(argv);
